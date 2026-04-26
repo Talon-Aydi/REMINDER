05e96c843532b615bc386712f5459ecef2ed7086
Talon <ak1998@live.nl>
2026-04-26 02:23:46 +0200

    Readme updated Demo gifs added Names json added

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # REMINDER
+
 This application was created to refresh my knowledge of Laravel and Livewire, frameworks I worked with extensively in the past.
 
-#### Commands
+## Commands
+
 To run the project\
 `composer run dev`
+
+## Features
+
+**Current features that have been built into this application:**
+
+- A filter
+- An activity feed
+
+### Filter
+
+**Current features:**
+
+- Shows names matching to input
+
+**Demo**
+![Demo](assets/demo/filter.gif)
+
+### Activity
+
+**Current features:**
+
+- Create a new activity
+- Delete an existing activity
+- Showing all the activities
+
+**Future implementations:**
+
+- Editing activity
+- Count down to activity dead-line
+- Adding color to activity
+- Pop-up reminder of scheduled activity
+
+![Demo](assets/demo/activity.gif)
--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -1,22 +1,13 @@
 <div class="w-screen h-screen flex flex-col justify-center">
-        <div class="flex flex-col w-[20rem] h-[25rem] justify-center text-center text-white m-auto space-y-2">
-            <span class="text-[2rem] font-bold">Filter component</span>
-            <input 
-                wire:model.live.debounce.250ms="namefield"
-                type="text" 
-                name="name" 
-                id="name" 
-                class="w-full h-[3rem] text-black bg-white px-3 border-0 rounded-sm" 
-                placeholder="Jane Doe"
-                >
+    <div class="flex flex-col w-[20rem] h-[25rem] justify-center text-center text-white m-auto space-y-2">
+        <span class="text-[2rem] font-bold">Name list</span>
+        <input wire:model.live.debounce.250ms="namefield" type="text" name="name" id="name"
+            class="w-full h-[3rem] text-black bg-white px-3 border-0 rounded-sm" placeholder="Jane Doe">
 
-            <div class="bg-white flex flex-col w-[20rem] h-[10rem] text-left px-3 py-3 space-y-2 rounded-sm overflow-auto">
-                @foreach ($nameList as $name)
-                    <span 
-                    wire:key="name-{{ $name }}"
-                    wire:transition
-                    class="text-black">{{ $name }}</span>
-                @endforeach
-            </div>
+        <div class="bg-white flex flex-col w-[20rem] h-[10rem] text-left px-3 py-3 space-y-2 rounded-sm overflow-auto">
+            @foreach ($nameList as $name)
+                <span wire:key="name-{{ $name }}" wire:transition class="text-black">{{ $name }}</span>
+            @endforeach
         </div>
     </div>
+</div>
