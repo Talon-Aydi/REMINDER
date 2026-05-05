ea75b30dab0e080491ce237d6dea6d4c4edff8cc
Talon <ak1998@live.nl>
2026-05-05 03:42:36 +0200

    Status can now be updated

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['activity_title', 'activity_description', 'activity_completion', 'activity_deadline', 'activity_user_id'])]
 class Activity extends Model
 {
     protected $table = 'activity';
@@ -12,13 +14,5 @@ class Activity extends Model
 
     protected $casts = [
         'activity_deadline' => 'datetime',
-    ];
-
-    protected $fillable = [
-        'activity_title',
-        'activity_description',
-        'activity_completion',
-        'activity_deadline',
-        'activity_user_id',
     ];
 }
--- resources/views/livewire/user/dashboard/dashboard.blade.php
+++ resources/views/livewire/user/dashboard/dashboard.blade.php
@@ -2,7 +2,7 @@
 <div class="flex flex-col justify-center items-stretch">
     <div class="pt-5 pb-5 flex-col flex">
         <span class="text-[24px] font-extrabold"> Dashboard </span>
-        <span> Welcome back, {{ $user-> name}}! </span>
+        <span> Welcome back, {{ $user->name}}! </span>
     </div>
 
     <div
@@ -47,12 +47,13 @@
             <div
                 class="flex flex-col bg-white rounded-2xl h-full w-full p-5 space-y-2"
             >
-                <span class="font-extrabold"> Status </span>
+                <span class="font-extrabold"> Bio </span>
                 <div class="group flex flex-row items-stretch">
                     <input
+                        wire:keydown.enter="update"
+                        wire:model="bio"
                         type="text"
                         class="italic p-3 w-full outline-none"
-                        value="A profound quote about happiness."
                     />
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 mt-auto mb-auto opacity-0 group-hover:opacity-100 group-focus-within:opacity-100">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
